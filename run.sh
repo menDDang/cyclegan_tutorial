c26b930a48c3788b55dda1418839753a0d47250b
menDDang <menddang2357@gmail.com>
2020-10-10 16:54:21 +0900

    fix path

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-work_dir="/home/kmyoon/PycharmProjects/CycleGAN"
+work_dir="/home/kmyoon/exp/gan/cyclegan"
 
 # step 1. 데이터 준비
-if [ ! -d $work_dir/vangogh2photo ]; then
+if [ ! -d $work_dir/datasets/vangogh2photo ]; then
   echo "There is no data set!"
-  sh $work_dir/datasets/download_datasets.sh
+  bash $work_dir/scripts/download_dataset.sh || exit 1
 fi
 
